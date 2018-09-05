0d74c49877c236615c60387be93b5b596c663528
JuniorBonilla <juniorbonilla100@gmail.com>
2018-09-05 08:00:10 -0500

    inicio_2

---
+++ app/src/main/java/com/example/bautista/pruebacartago/inicio.java
@@ -0,0 +1,13 @@
+package com.example.bautista.pruebacartago;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
