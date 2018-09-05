cde200899ecf161d9307bef665c585099ca30afe
Bautista <jabautista065@misena.edu.co>
2018-09-05 07:27:55 -0500

    Navigation Drawer y Splash

--- app/src/main/java/com/example/bautista/pruebacartago/Splash.java
+++ app/src/main/java/com/example/bautista/pruebacartago/Splash.java
@@ -1,5 +1,7 @@
 package com.example.bautista.pruebacartago;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent Menu = new Intent(Splash.this, Navigation.class );
+                startActivity(Menu);
+                finish();
+            }
+        },10000);
     }
 }
